a2fd1727392ee6131da9c54894c8548eddbe3e8a
Jo Lily <lily@example.com>
2026-04-19 00:11:04 +0900

    headコマンド実装(POSIXに概ね準拠)

---
+++ head/main.go
@@ -0,0 +1,93 @@
+package main
+
+import (
+	"bufio"
+	"flag"
+	"fmt"
+	"io"
+	"os"
+)
+
+type Flags struct {
+	c int
+	n int
+}
+
+func parseFlags(args []string) (Flags, []string, int, error) {
+	var flagOptions Flags
+	fs := flag.NewFlagSet("head", flag.ContinueOnError)
+	fs.IntVar(&flagOptions.c, "c", 0, "option specify bytes.")
+	fs.IntVar(&flagOptions.n, "n", 10, "option specify lines.")
+	err := fs.Parse(args)
+	return flagOptions, fs.Args(), fs.NFlag(), err
+}
+
+func main() {
+	flags, args, nFlag, err := parseFlags(os.Args[1:])
+	if err != nil {
+		fmt.Fprintln(os.Stderr, "head: ", err)
+		os.Exit(1)
+	}
+	if nFlag >= 2 {
+		fmt.Fprintln(os.Stderr, "head: cannot combine line and bytes")
+		os.Exit(1)
+	}
+
+	fileNameList := args
+
+	for _, fileName := range fileNameList {
+		rd, err := os.Open(fileName)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "head: ", err)
+			os.Exit(1)
+		}
+
+		err = processFile(rd, flags)
+		if err != nil {
+			fmt.Fprintln(os.Stderr, "head: ", err)
+			os.Exit(1)
+		}
+		rd.Close()
+	}
+}
+
+func processFile(rd io.Reader, flags Flags) error {
+	if flags.c != 0 {
+		reader := bufio.NewReader(rd)
+		result, err := readBytes(reader, flags.c)
+		if err != nil {
+			return err
+		}
+		fmt.Print(result)
+		return nil
+	}
+
+	scanner := bufio.NewScanner(rd)
+	resultLines := readLines(scanner, flags.n)
+	for _, line := range resultLines {
+		fmt.Println(line)
+	}
+
+	return nil
+}
+
+func readBytes(reader io.Reader, bytes int) (string, error) {
+	rdBytes := make([]byte, bytes)
+	data, err := io.ReadFull(reader, rdBytes)
+	if err != nil && err != io.ErrUnexpectedEOF {
+		return "", err
+	}
+
+	return string(rdBytes[:data]), nil
+}
+
+func readLines(scanner *bufio.Scanner, lineLimit int) []string {
+	result := []string{}
+	for i := 0; i < lineLimit; i++ {
+		if !scanner.Scan() {
+			break
+		}
+		result = append(result, scanner.Text())
+	}
+	return result
+}
